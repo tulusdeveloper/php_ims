c3540da4d555b9c90fad88a9121d1fa0ce374097
TULUS <tulus@192.168.1.19>
2022-11-30 13:38:16 +0300

    Changes

--- admin/forajax/load_product_using_company.php
+++ admin/forajax/load_product_using_company.php
@@ -4,9 +4,8 @@ $company_name=$_GET["company_name"];
 $res=mysqli_query($link,"select * from products where company_name='$company_name'");
 
 ?>
-<div class="control-group">
-<select class="" name="product_name" id="product_name" onchange="select_product(this.value,'<?php echo $company_name ?>')">
-<option class="span11">Select</option>
+<select class="span11" name="product_name" id="product_name" onchange="select_product(this.value,'<?php echo $company_name ?>')">
+<option>Select</option>
 </div>
 <?php
 while($row=mysqli_fetch_array($res))
